2474a25dc3e2d97fa000925fa9d0b0beffb6a13a
code-horse-mi <945623397@qq.com>
2024-03-02 20:32:32 +0800

    create chat

--- apps/all/internal.go
+++ apps/all/internal.go
@@ -2,7 +2,9 @@ package all
 
 import (
 	_ "github.com/solodba/ichatgpt/apps/audio/impl"
+	_ "github.com/solodba/ichatgpt/apps/chat/impl"
 	_ "github.com/solodba/ichatgpt/apps/file/impl"
+	_ "github.com/solodba/ichatgpt/apps/finetune/impl"
 	_ "github.com/solodba/ichatgpt/apps/image/impl"
 	_ "github.com/solodba/ichatgpt/apps/model/impl"
 )
